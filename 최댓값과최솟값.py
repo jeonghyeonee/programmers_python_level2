60a2c5e99c1f1500e933ce0a682ceb53543814d0
jeonghyeonee <jlh1218@naver.com>
2022-03-02 01:11:14 +0900

    2022.03.01.

--- 최댓값과최솟값.py
+++ 최댓값과최솟값.py
@@ -1,4 +1,3 @@
 def solution(s):
-    arr = list(input())     #'-1'과 같은 경우에는 어떻게 받아서 숫자로 변형하지...?
-    answer = ''
-    return answer
+    arr = list(map(int, s.split(' ')))     #'-1'과 같은 경우에는 어떻게 받아서 숫자로 변형하지...?
+    return '{} {}'.format(min(arr), max(arr))
