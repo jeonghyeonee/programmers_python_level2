428dc05ab549872df1f6fdcce2afd591df27f005
jeonghyeonee <jlh1218@naver.com>
2022-03-05 23:09:45 +0900

    2022.03.05.

--- 프린터.py
+++ 프린터.py
@@ -1,3 +1,12 @@
+# queue 관련으로 풀 수 있지 않을까????
 def solution(priorities, location):
     answer = 0
-    return answer
+    l = len(priorities)
+    dic = {}
+    for i in range(l):
+        dic[chr(65+i)] = priorities[i]
+    
+    
+    return dic
+
+print(solution([2,1,3,2], 2))
