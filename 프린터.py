19221f9c5bc1be638aa9dc0febd69c594b851adf
jeonghyeonee <jlh1218@naver.com>
2022-03-22 23:37:54 +0900

    2022.03.22.

--- 프린터.py
+++ 프린터.py
@@ -1,12 +1,23 @@
+# https://coding-grandpa.tistory.com/98 
+
 # queue 관련으로 풀 수 있지 않을까????
 def solution(priorities, location):
+    arr = [(i, p) for i,p in enumerate(priorities)]
     answer = 0
-    l = len(priorities)
-    dic = {}
-    for i in range(l):
-        dic[chr(65+i)] = priorities[i]
     
+    while arr:
+        job = arr.pop(0)
+        
+        if any(job[1] < other_job[1] for other_job in arr):
+            arr.append(job)
+        
+        else:
+            answer += 1
+            
+            if job[0] == location:
+                break;
     
-    return dic
+    return answer
+    
 
 print(solution([2,1,3,2], 2))
